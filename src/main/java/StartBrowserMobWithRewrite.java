89b7f933c35e239be12576e6549450c4aa079f17
sauceaaron <aaron.evans@saucelabs.com>
2019-10-15 16:43:14 -0600

    create project

---
+++ src/main/java/StartBrowserMobWithRewrite.java
@@ -0,0 +1,43 @@
+import io.netty.handler.codec.http.HttpRequest;
+import io.netty.handler.codec.http.HttpResponse;
+import net.lightbody.bmp.BrowserMobProxy;
+import net.lightbody.bmp.BrowserMobProxyServer;
+import net.lightbody.bmp.filters.RequestFilter;
+import net.lightbody.bmp.util.HttpMessageContents;
+import net.lightbody.bmp.util.HttpMessageInfo;
+
+import java.util.Scanner;
+
+public class StartBrowserMobWithRewrite
+{
+	public static void main(String[] args)
+	{
+		System.out.println("starting browsermob proxy");
+		BrowserMobProxy proxy = new BrowserMobProxyServer();
+		proxy.setTrustAllServers(true);
+//		proxy.rewriteUrl("http://foo.test:9080/foo.html", "http://bar.test:9080/bar.html");
+
+		proxy.addRequestFilter(new RequestFilter()
+		{
+			@Override
+			public HttpResponse filterRequest(HttpRequest request, HttpMessageContents message, HttpMessageInfo info)
+			{
+				System.out.println("Original request URL: " + request.getUri());
+				request.setUri(request.getUri().replaceAll("foo", "bar"));
+				System.out.println("Changed request URL: " + request.getUri());
+
+				return null;
+			}
+		});
+
+		proxy.start(55555);
+		int port = proxy.getPort();
+		System.out.println("started proxy server on " + " http://localhost:" + port);
+
+		while(true)
+		{
+			try { Thread.sleep(10000); System.out.println(".");}
+			catch (InterruptedException e) { e.printStackTrace(); }
+		}
+	}
+}
